39becaa006f6341b4542603d792f1c137e0c19d4
Juan Pedro Medina Silva <64823850+juanpemedina@users.noreply.github.com>
2024-10-28 12:30:52 -0600

    duodecima transformacion Limpieza General del Módulo

--- gilded_rose/lib/gilded_rose.ex
+++ gilded_rose/lib/gilded_rose.ex
@@ -1,8 +1,4 @@
 defmodule GildedRose do
-  # Example
-  # update_quality([%Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 9, quality: 1}])
-  # => [%Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 8, quality: 3}]
-
   def update_quality(items) do
     Enum.map(items, &update_item/1)
   end
@@ -20,7 +16,7 @@ defmodule GildedRose do
   end
 
   defp update_quality_for_item(%{name: "Backstage passes to a TAFKAL80ETC concert"} = item) do
-    handle_special_items(item)
+    update_backstage_passes(item)
   end
 
   defp update_quality_for_item(item), do: degrade_quality(item, 1)
@@ -28,10 +24,7 @@ defmodule GildedRose do
   defp update_sell_in(%{name: "Sulfuras, Hand of Ragnaros"} = item), do: item
   defp update_sell_in(item), do: %{item | sell_in: item.sell_in - 1}
 
-  defp handle_sell_in_expiration(item) when item.sell_in < 0 do
-    handle_expired(item)
-  end
-
+  defp handle_sell_in_expiration(item) when item.sell_in < 0, do: handle_expired(item)
   defp handle_sell_in_expiration(item), do: item
 
   defp degrade_quality(item, amount) when item.name == "Sulfuras, Hand of Ragnaros", do: item
@@ -39,7 +32,6 @@ defmodule GildedRose do
   defp degrade_quality(item, amount) do
     %{item | quality: item.quality - amount}
   end
-
 
   defp increase_quality(item, amount \\ 1) do
     %{item | quality: min(item.quality + amount, 50)}
@@ -58,29 +50,14 @@ defmodule GildedRose do
   defp increase_quality_if_expired(item) when item.sell_in < 6, do: increase_quality(item)
   defp increase_quality_if_expired(item), do: item
 
-
-  defp handle_expired(%{name: "Aged Brie"} = item), do: handle_aged_brie(item)
-  defp handle_expired(%{name: "Backstage passes to a TAFKAL80ETC concert"} = item), do: handle_backstage_passes(item)
-  defp handle_expired(item) when item.quality > 0, do: handle_normal_item(item)
+  defp handle_expired(%{name: "Aged Brie"} = item), do: increase_quality(item)
+  defp handle_expired(%{name: "Backstage passes to a TAFKAL80ETC concert"} = item), do: %{item | quality: 0}
+  defp handle_expired(item) when item.quality > 0, do: degrade_quality(item, 1)
   defp handle_expired(item), do: item
-
-  defp handle_aged_brie(item) do
-    increase_quality(item)
-  end
-
-  defp handle_backstage_passes(item) do
-    %{item | quality: 0}
-  end
-
-  defp handle_normal_item(item) do
-    degrade_quality(item, 1)
-  end
 
   defp handle_special_items(%{name: "Aged Brie"} = item), do: increase_quality(item)
   defp handle_special_items(%{name: "Backstage passes to a TAFKAL80ETC concert"} = item), do: update_backstage_passes(item)
   defp handle_special_items(item), do: degrade_quality(item, 1)
 
-  defp should_decrease_sell_in?(item) do
-    item.name != "Sulfuras, Hand of Ragnaros"
-  end
+  defp should_decrease_sell_in?(item), do: item.name != "Sulfuras, Hand of Ragnaros"
 end

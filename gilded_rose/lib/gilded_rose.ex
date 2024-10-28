31a7a6403dfa32a73cc0b4e44dfb25ddafcc4714
Juan Pedro Medina Silva <64823850+juanpemedina@users.noreply.github.com>
2024-10-28 12:22:20 -0600

    Undecima transformacion

--- gilded_rose/lib/gilded_rose.ex
+++ gilded_rose/lib/gilded_rose.ex
@@ -8,10 +8,10 @@ defmodule GildedRose do
   end
 
   def update_item(item) do
-    item = update_quality_for_item(item)
-    item = update_sell_in(item)
-    item = handle_sell_in_expiration(item)
     item
+    |> update_quality_for_item()
+    |> update_sell_in()
+    |> handle_sell_in_expiration()
   end
 
   defp update_quality_for_item(%{name: "Aged Brie", quality: quality} = item) do
@@ -61,11 +61,8 @@ defmodule GildedRose do
 
 
   defp handle_expired(%{name: "Aged Brie"} = item), do: handle_aged_brie(item)
-
   defp handle_expired(%{name: "Backstage passes to a TAFKAL80ETC concert"} = item), do: handle_backstage_passes(item)
-
   defp handle_expired(item) when item.quality > 0, do: handle_normal_item(item)
-
   defp handle_expired(item), do: item
 
   defp handle_aged_brie(item) do
@@ -81,11 +78,8 @@ defmodule GildedRose do
   end
 
   defp handle_special_items(%{name: "Aged Brie"} = item), do: increase_quality(item)
-
   defp handle_special_items(%{name: "Backstage passes to a TAFKAL80ETC concert"} = item), do: update_backstage_passes(item)
-
   defp handle_special_items(item), do: degrade_quality(item, 1)
-
 
   defp should_decrease_sell_in?(item) do
     item.name != "Sulfuras, Hand of Ragnaros"
